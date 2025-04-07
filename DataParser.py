8447ba214079c40d322ac3865958686d2c19bb19
Randz <158979362+Randz0@users.noreply.github.com>
2025-04-07 14:07:16 -0400

    Update DataParser.py
    Moved alot of the cached file functionality into the cached file class to better encapsulate the data, and make modding easier. Simply create your own custom implementation of the Cached File class and it will retain all of the features that are by default in the program and then anything else your own custom commands act on (also inherited)

--- DataParser.py
+++ DataParser.py
@@ -1,62 +1,144 @@
-editedOutPunctuation = {"." : " ", "," : " ", "!" : " ", ":" : " ", ";" : " ", "?" : " ", "\n" : " "} # Dictionary used to filter out punctuation from the search result
+editedOutPunctuation = {"." : " ", "," : " ", "!" : " ", ":" : " ", ";" : " ", "?" : " ", "\n" : " "}
 
-class Cached:
-    currentTokenizedWords = None
+class CachedFile:
+    def __init__(self, fileName):
+        self.fileName = fileName
+        self.currentTokenizedWords = []
 
-    defaultWordsToFrequency = {}
-    allLowerWordsToFrequency = {}
+        self.isCaseSensitive = True
 
-    byFreqWordsToFrequency = []
-    allLowerByFreqWordsToFreq = []
+        self.wordsToFrequency = {}
+        self.allLowerWordsToFrequency = {}
 
-    currentPlotBounds = (0, 0)
+        self.byFreqWordsToFrequency = [] # Both of these data structures are list of tuples (word --> ocurrences)
+        self.allLowerByFreqWordsToFreq = []
 
-# The input *allLines* should be a list containing all the lines of the input text file as strings
-def CacheTokenizedFileAsWords(allFileLines):
-    Cached.currentTokenizedWords = [] # Create empty list that will be populated with the individual words
+        self.byFreqWordsToFreqInUse = []
+
+        self.plotBounds = (0, 0)
+
+        self.blacklist = []
+        self.allLowerBlacklist = []
+
+        self.blacklistInUse = []
+
+    def GetAppearencesOfWord(self, wordLookingFor: str):
+        listSearchingFrom = self.wordsToFrequency if self.isCaseSensitive else self.allLowerWordsToFrequency
+        wordLookingFor = wordLookingFor if self.isCaseSensitive else wordLookingFor.lower()
+
+        if not wordLookingFor in listSearchingFrom:
+            return 0
+
+        return listSearchingFrom[wordLookingFor]
+
+    def WordIsInOutputData(self, word):
+        return not word in self.blacklistInUse
+
+    def FetchWordsToFrequencies(self):
+            self.byFreqWordsToFreqInUse = self.byFreqWordsToFrequency if self.isCaseSensitive else self.allLowerByFreqWordsToFreq
+            self.blacklistInUse = self.blacklist if self.isCaseSensitive else self.allLowerBlacklist
+
+            wordsWithinBounds = []
+            frequenciesWithinBounds = []
+
+            numberOfWordsToFetch = self.plotBounds[1] - self.plotBounds[0]
+
+            index = self.plotBounds[0] - 1
+            stopIndex = self.plotBounds[0] + numberOfWordsToFetch
+
+            currentWordFreqPair = ("", 0)
+
+            while index < stopIndex:
+                index += 1
+
+                if index >= len(self.byFreqWordsToFreqInUse):
+                    break
+
+                currentWordFreqPair = self.byFreqWordsToFreqInUse[index]
+
+                if self.WordIsInOutputData( currentWordFreqPair[0] ):
+                    wordsWithinBounds.append( currentWordFreqPair[0] )
+                    frequenciesWithinBounds.append( currentWordFreqPair[1] )
+                else:
+                    stopIndex += 1
+
+            return (wordsWithinBounds, frequenciesWithinBounds)
+
+    def CacheOrganizedWordFrequencyLists(self):
+        self.byFreqWordsToFrequency.clear()
+        self.byFreqWordsToFrequency.extend(sorted(self.wordsToFrequency.items(), key=lambda item: item[1], reverse=True))
+
+        self.allLowerByFreqWordsToFreq.clear()
+        self.allLowerByFreqWordsToFreq.extend(sorted(self.allLowerWordsToFrequency.items(), key=lambda item: item[1], reverse=True))
+
+    def CountIndividualWordIntoCache(self, word: str):
+        if word in self.wordsToFrequency:
+            self.wordsToFrequency[word] += 1 # Add one more occurence
+        else:
+            self.wordsToFrequency.update({word : 1}) # Add first instance
+
+        if word.lower() in self.allLowerWordsToFrequency:
+            self.allLowerWordsToFrequency[word.lower()] += 1 # Add one more occurence
+        else:
+            self.allLowerWordsToFrequency.update({word.lower() : 1}) # Add first instance
+
+    def CacheWordFrequencies(self):
+        for i in range(len(self.currentTokenizedWords)):
+            self.CountIndividualWordIntoCache(self.currentTokenizedWords[i])
+
+    def CacheLinesIntoTokenizedWords(self, allFilelines):
+        self.currentTokenizedWords.clear() # Create empty list that will be populated with the individual words
     
-    for i in range(len(allFileLines)):
-        line = allFileLines[i]
+        for i in range(len(allFilelines)):
+            line = allFilelines[i]
+
+            line = str.translate(line, str.maketrans(editedOutPunctuation))
+            line = str.split(line) # The line has been divied up into an array of the individual words
+
+            for j in range(len(line)):
+                self.currentTokenizedWords.append(line[j])
+
+    def CacheTokenizedFileIntoBlacklist(self, tokenizedFile: list[str]):
+        self.blacklist.clear()
+
+        for i in range(len(tokenizedFile)):
+            self.blacklist.append(tokenizedFile[i])
+
+            self.allLowerBlacklist.append(tokenizedFile[i].lower())
+
+cachedFiles: list[CachedFile] = []
+
+def GetNextFileVersion(baseName):
+    fileVersion = 0
+
+    for file in cachedFiles:
+        if file.fileName.startswith(baseName):
+            fileVersion += 1
+
+    return "" if fileVersion == 0 else str(fileVersion)
+
+def CreateDefaultCachedFile(fileName):
+    cachedFiles.append(CachedFile( fileName + GetNextFileVersion(fileName) ))
+
+    return cachedFiles[len(cachedFiles) - 1]
+
+def GetCachedFileByName(name):
+    for file in cachedFiles:
+        if file.fileName == name:
+            return file
+        
+    return None
+
+def TokenizeFileFromLines(allLines):
+    tokenizedContent = []
+
+    for i in range(len(allLines)):
+        line = allLines[i]
 
         line = str.translate(line, str.maketrans(editedOutPunctuation))
         line = str.split(line) # The line has been divied up into an array of the individual words
 
         for j in range(len(line)):
-            Cached.currentTokenizedWords.append(line[j])
-
-def CountIndividualWordIntoCache(word):
-    if word in Cached.defaultWordsToFrequency:
-        Cached.defaultWordsToFrequency[word] += 1 # Add one more occurence
-    else:
-        Cached.defaultWordsToFrequency.update({word : 1}) # Add first instance
-
-    if word.lower() in Cached.allLowerWordsToFrequency:
-        Cached.allLowerWordsToFrequency[word.lower()] += 1 # Add one more occurence
-    else:
-        Cached.allLowerWordsToFrequency.update({word.lower() : 1}) # Add first instance
-
-# expects the input to already be tokenized word by word
-def CacheWordFrequencies(tokenizedInput):
-    for i in range(len(tokenizedInput)): # Loop through every word
-        CountIndividualWordIntoCache(tokenizedInput[i])
-
-# Stores the word ocurrenences dictionary as a collection of tuples based on the frequency of each word from most to least ocurring
-def CacheOrganizedWordFrequencyList(cachingFrom, cachingTo):
-    cachingTo.clear()
-    cachingTo.extend(sorted(cachingFrom.items(), key=lambda item: item[1], reverse=True))
-
-# Returns a tuple of the words and their frequency only returning up to the depth most ocurring word 
-def FetchWordsInFromPlotBounds(fetchingFrom):
-    wordsWithinBounds = []
-    frequenciesWithinBounds = []
-
-    numberOfWordsToFetch = Cached.currentPlotBounds[1] - Cached.currentPlotBounds[0]
-
-    for i in range(numberOfWordsToFetch):
-        if i >= len(fetchingFrom):
-            break
-
-        wordsWithinBounds.append(fetchingFrom[i + Cached.currentPlotBounds[0]][0])
-        frequenciesWithinBounds.append(fetchingFrom[i + Cached.currentPlotBounds[0]][1])
-
-    return (wordsWithinBounds, frequenciesWithinBounds)
+            tokenizedContent.append(line[j])
+    
+    return tokenizedContent
